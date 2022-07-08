a00dc73bc0153674576a7bb3080e9e4e38044394
Joydip Roy <joydipand@gmail.com>
2022-07-08 09:59:04 +0000

    remove: tsup config, added typecheck, added devcontainer

--- parcel.d.ts
+++ parcel.d.ts
@@ -1,10 +1,10 @@
-import preact from 'preact'
+import * as preact from 'preact'
 import { JSXInternal } from 'preact/src/jsx'
 
 declare module 'preact/src/jsx' {
-    namespace JSXInternal {
-	interface IntrinsicElements {
-	    ["py-script"]: Partial<HTMLElement & { children: any}>
+	namespace JSXInternal {
+		interface IntrinsicElements {
+			["py-script"]: Partial<HTMLElement & { children: any }>
+		}
 	}
-    }
 }
